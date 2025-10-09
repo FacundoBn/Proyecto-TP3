1563dd5c2d19a5ce333ebd53db0c800db25d4c34
Melany Meichtri <62313691+mmeichtri@users.noreply.github.com>
2025-10-09 15:56:20 -0300

    Update ticket.dart

--- lib/models/ticket.dart
+++ lib/models/ticket.dart
@@ -8,9 +8,7 @@ class TicketView {
   final String userId;
   final String? guestId;
 
-  bool get activo => egreso == null;
-
-  TicketView({
+  const TicketView({
     required this.id,
     required this.patente,
     required this.slotId,
@@ -20,4 +18,28 @@ class TicketView {
     required this.userId,
     required this.guestId,
   });
+
+  bool get activo => egreso == null;
+
+  TicketView copyWith({
+    String? id,
+    String? patente,
+    String? slotId,
+    DateTime? ingreso,
+    DateTime? egreso,
+    num? precioFinal,
+    String? userId,
+    String? guestId,
+  }) {
+    return TicketView(
+      id: id ?? this.id,
+      patente: patente ?? this.patente,
+      slotId: slotId ?? this.slotId,
+      ingreso: ingreso ?? this.ingreso,
+      egreso: egreso ?? this.egreso,
+      precioFinal: precioFinal ?? this.precioFinal,
+      userId: userId ?? this.userId,
+      guestId: guestId ?? this.guestId,
+    );
+  }
 }
